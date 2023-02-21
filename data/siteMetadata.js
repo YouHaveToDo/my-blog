a90ffbfe7b1585e5dd7919ce9c28b98fa84bb941
HaveToDo <zmfpdkwl2@gmail.com>
2023-02-21 23:33:57 +0900

    Doc: mod siteMetaData

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,22 +1,22 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
-  language: 'en-us',
+  title: '움직이지 않으면 결과도 없다.',
+  author: 'YouHaveToDo',
+  headerTitle: 'YouHaveToDoBlog',
+  description: '내가 공부한 것을 공유합니다.',
+  language: 'kr',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://www.youhavetodo.xyz',
+  siteRepo: 'https://github.com/youHaveToDo/my-blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
-  socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
+  socialBanner: '/static/images/google.png',
+  email: 'zmfpdkwl2@gmail.com',
+  github: 'https://github.com/youHaveToDo',
   twitter: 'https://twitter.com/Twitter',
   facebook: 'https://facebook.com',
   youtube: 'https://youtube.com',
   linkedin: 'https://www.linkedin.com',
-  locale: 'en-US',
+  locale: 'kr',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
